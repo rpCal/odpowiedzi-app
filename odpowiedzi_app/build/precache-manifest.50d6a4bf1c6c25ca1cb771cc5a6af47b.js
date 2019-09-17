3b3ea5a0bab5dd3e758cf3b463419b804b55907c
CalkaRob <calka.rob@gmail.com>
2019-09-17 19:21:03 +0200

    up

---
+++ odpowiedzi_app/build/precache-manifest.50d6a4bf1c6c25ca1cb771cc5a6af47b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4430640ca38cc8d69227a8a200ce987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b8f85901a9ca09593bf",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "13b2b3a0d66aa744b118",
+    "url": "/static/js/2.dc6d954a.chunk.js"
+  },
+  {
+    "revision": "5b8f85901a9ca09593bf",
+    "url": "/static/js/main.2c7cd121.chunk.js"
+  },
+  {
+    "revision": "e98707654e0e545f78b1",
+    "url": "/static/js/runtime~main.31929e00.js"
+  }
+]);
